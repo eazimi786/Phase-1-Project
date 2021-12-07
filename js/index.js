a448e1523f7e9f9b9745a329e1d1a974db175790
Alexandra Weaver <aweaver0124@gmail.com>
2021-12-07 13:56:04 -0600

    Added minimal javascript methods

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
+let likeCount = 0 + 1
+
+// document.addEventListener('DOMContentLoaded', function(){
+// fetchCoffee()
+// })
+
 console.log ("coffee")
+
+// function fetchCoffee(){
+// fetch('https://api.sampleapis.com/coffee/hot')
+// .then(resp => resp.json())
+// .then(data => {
+//     console.log(data)
+// })
+// }
+function likeAnimation(){
+document.querySelector('.upvote-button').addEventListener('click', increaseLikes())
+}
+
+function increaseLikes(e){
+    document.querySelector('.upvote-count').textContent = likeCount + ' Upvotes'
+    e.target = likeCount++
+}
