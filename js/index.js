4c451da45ae41467ab228c34e8d986b92143fc59
Alexandra Weaver <aweaver0124@gmail.com>
2021-12-08 14:27:37 -0600

    added a list of all coffees so users can search their names

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(){
 fetchHotCoffee()
 fetchIcedCoffee()
+fetchAllHotCoffee()
+fetchAllIcedCoffee()
 })
 
 function fetchHotCoffee(){
 fetch('https://api.sampleapis.com/coffee/hot')
 .then(resp => resp.json())
 .then(data => {
-    console.log(data)
+    
     // First Card
     document.querySelector('#coffee-1-title').textContent = data[14].title
     document.querySelector('#description-1').textContent = data[14].description
@@ -24,11 +26,70 @@ function fetchIcedCoffee(){
 fetch('https://api.sampleapis.com/coffee/iced/')
 .then(resp => resp.json())
 .then(data => {
-    console.log(data)
-    // Third Card
-
+   
+     // Third Card
     document.querySelector('#coffee-3-title').textContent = data[2].title
     document.querySelector('#description-3').textContent = data[2].description
     document.querySelector('#ingredients-3').textContent = data[2].ingredients
 })
 }
+
+function fetchAllHotCoffee(){
+    fetch('https://api.sampleapis.com/coffee/hot')
+    .then(resp => resp.json())
+    .then(data => {
+        data.forEach(obj => {
+            const p = document.createElement('p');
+            p.className = 'coffee-names';
+            p.textContent = obj.title
+            document.querySelector('#hot-coffee-list').append(p)
+        })
+    })
+}
+function fetchAllIcedCoffee(){
+    fetch('https://api.sampleapis.com/coffee/iced/')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(obj => {
+            const p = document.createElement('p');
+            p.className = 'coffee-names';
+            p.textContent = obj.title
+            document.querySelector('#iced-coffee-list').append(p);
+        })
+        
+    })
+}
+
+// Likes
+let likeCountOne = 0;
+let likeCountTwo = 0;
+let likeCountThree = 0;
+   
+document.querySelector('#button-1').addEventListener('click', (e) => {
+    e.target = likeCountOne++
+    document.querySelector('#upvote-count-1').textContent = likeCountOne + " Upvotes"
+})
+
+document.querySelector('#button-2').addEventListener('click', (e) => {
+    e.target = likeCountTwo++
+    document.querySelector('#upvote-count-2').textContent = likeCountTwo + " Upvotes"
+})
+
+document.querySelector('#button-3').addEventListener('click', (e) => {
+    e.target = likeCountThree++
+    document.querySelector('#upvote-count-3').textContent = likeCountThree + " Upvotes"
+})
+
+// Comments - Add and Remove
+document.querySelector('#comment-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const li = document.createElement('li');
+    li.textContent = comment.value
+    document.querySelector('#comments').append(li)
+    li.addEventListener('click', e =>{
+        e.target.remove()
+    })
+})
+
+// Adding Your Own Coffee
+
